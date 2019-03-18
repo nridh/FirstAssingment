02241e4a207f132de69253376ae4ddb11eb984b5
nridh <48426835+nridh@users.noreply.github.com>
2019-03-18 18:08:57 +0800

    Update server.java

--- server.java
+++ server.java
@@ -1,57 +1,61 @@
-import java.io.*;
-import java.net.*;
-
-class server 
-{
-    public static void main(String[] args)
-
-       {
-        int port = 3222; 
-        try 
-          {
-            ServerSocket ss = new ServerSocket(port);
-            System.out.println("Waiting for a client....");
-
-            System.out.println("Client:");
-            System.out.println();
-            while(true) {
-            Socket socket = ss.accept();
-
-                SSocket sSocket = new SSocket(socket);
-                Thread t = new Thread(sSocket);
-                t.start();
-          System.out.println("Socket Stack Size-----"+socketMap.size());
-            }
-        }
-        catch (Exception e) { }
-    }
-}
-
-class SSocket implements Runnable {
-    private Socket socket;
-
-    public SSocket(Socket socket) {
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        try {
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
-
-            DataInputStream dIn = new DataInputStream(in);
-            DataOutputStream dOut = new DataOutputStream(out);
-
-            String line = null;
-            while (true) {
-                line = dIn.readUTF();
-                System.out.println("Recievd the line----" + line);
-                dOut.writeUTF(line + " Comming back from the server");
-                dOut.flush();
-                System.out.println("waiting for the next line....");    
-            }
-        }
-        catch (Exception e) { }
-    }
-}
+import java.net.*; 
+import java.io.*; 
+  
+public class server 
+{ 
+    //initialize socket and input stream 
+    private Socket          socket   = null; 
+    private ServerSocket    server   = null; 
+    private DataInputStream in       =  null; 
+  
+    // constructor with port 
+    public Server(int port) 
+    { 
+        // starts server and waits for a connection 
+        try
+        { 
+            server = new ServerSocket(port); 
+            System.out.println("Server started"); 
+  
+            System.out.println("Waiting for a client ..."); 
+  
+            socket = server.accept(); 
+            System.out.println("Client accepted"); 
+  
+            // takes input from the client socket 
+            in = new DataInputStream( 
+                new BufferedInputStream(socket.getInputStream())); 
+  
+            String line = ""; 
+  
+            // reads message from client until "Over" is sent 
+            while (!line.equals("Over")) 
+            { 
+                try
+                { 
+                    line = in.readUTF(); 
+                    System.out.println(line); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+            System.out.println("Closing connection"); 
+  
+            // close connection 
+            socket.close(); 
+            in.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Server server = new Server(3222); 
+    } 
+} 
