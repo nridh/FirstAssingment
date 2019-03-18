e7f0e95753fea0fb85e818486dc1d25020a86e13
nridh <48426835+nridh@users.noreply.github.com>
2019-03-18 17:54:05 +0800

    Update server.java

--- server.java
+++ server.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 class server 
 {
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
 
        {
         int port = 3222; 
@@ -21,6 +21,35 @@ class server
                 Thread t = new Thread(sSocket);
                 t.start();
           System.out.println("Socket Stack Size-----"+socketMap.size());
+            }
+        }
+        catch (Exception e) { }
+    }
+}
+
+class SSocket implements Runnable {
+    private Socket socket;
+
+    public SSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+
+            DataInputStream dIn = new DataInputStream(in);
+            DataOutputStream dOut = new DataOutputStream(out);
+
+            String line = null;
+            while (true) {
+                line = dIn.readUTF();
+                System.out.println("Recievd the line----" + line);
+                dOut.writeUTF(line + " Comming back from the server");
+                dOut.flush();
+                System.out.println("waiting for the next line....");    
             }
         }
         catch (Exception e) { }
