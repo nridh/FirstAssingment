27ef5352b003f59566a0179d45ab3284d65524e6
nridh <48426835+nridh@users.noreply.github.com>
2019-03-24 15:47:34 +0800

    Update server4.java

--- server4.java
+++ server4.java
@@ -8,21 +8,21 @@ class Server
     { 
         
         
-            server = new ServerSocket(3222); 
-            System.out.println("Server ready for chatting ~"); 
+            ServerSocket server = new ServerSocket(3222); 
+            System.out.println("    ~Server is ready ~"); 
   
             System.out.println("Waiting for a client ..."); 
   
-            socket = server.accept(); 
+            Socket server = sersock.accept(); 
             System.out.println("Client accepted"); 
   
             BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
             
            
             OutputStream ostream = server.getOutputStream(); 
-           PrintWriter pwrite = new PrintWriter(ostream, true);
-          InputStream istream = server.getInputStream();
-          BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+            PrintWriter pwrite = new PrintWriter(ostream, true);
+            InputStream istream = server.getInputStream();
+            BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
 
   
